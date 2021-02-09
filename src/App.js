bcb411385e000e0b5a2438cf6e48874cbefec02e
aribakhan97 <akhan41@binghamton.edu>
2021-02-09 13:31:25 -0500

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Compoenent {
+    render() {
+        return (
+            <div>
+                <h1>
+                App
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default App;
